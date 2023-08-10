0a3d5b250dde796df47f8f996dc86de21e7a920f
HadasHadad <123874368+HadasHadad@users.noreply.github.com>
2023-08-11 00:36:14 +0300

    tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: [ "./src/**/*.{css,html,js,jsx,ts,tsx}",
+  "./node_modules/flowbite/**/*.js",],
   theme: {
     extend: {},
   },
