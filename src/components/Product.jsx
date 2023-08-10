cae19e7f6a8b0bc86f821da0eab2440630465f03
HadasHadad <123874368+HadasHadad@users.noreply.github.com>
2023-08-11 01:18:08 +0300

    created a basic product page

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,11 +1,36 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
-import ProductCard from './ProductCard'
+import image from '../assets/images/f8b0f70b-e7a1-4437-af9e-dc8b529ec044.jpeg'
+
 
 export default function Product() {
   return (
     <>
-   <ProductCard/>
-   </>
-  )
+      <div className="flex justify-evenly items-center min-h-screen bg-gray-100">
+        <div className="w-full flex">
+          <div className="w-1/2 pr-8 ml-20">
+            <img src={image} alt="Product" className="w-full rounded-lg" />
+          </div>
+
+          <div className="w-1/2">
+            <h2 className="text-3xl font-semibold mb-4">שם המוצר</h2>
+
+            <p className="text-gray-700 text-lg mb-6">
+             תיאור המוצר
+            </p>
+            <h2>מחיר</h2>
+            <hr />
+
+            <div>
+                <p>משלוח חינם</p>
+              
+            </div>
+
+            <button className="bg-blue-500 text-white px-6 py-3 rounded-md hover:bg-blue-600">
+              הוספה לסל 
+            </button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -4,16 +4,30 @@ import image from '../assets/images/f8b0f70b-e7a1-4437-af9e-dc8b529ec044.jpeg'
 export default function ProductCard() {
   return (
     <>
-    <div className="card w-96 bg-base-100 shadow-xl">
-  <figure><img src={image} alt="Shoes" /></figure>
-  <div className="card-body">
-    <h2 className="card-title">Shoes!</h2>
-    <p>If a dog chews shoes whose shoes does he choose?</p>
-    <div className="card-actions justify-end">
-      <button className="btn btn-primary">Buy Now</button>
+      <div className="flex items-center justify-center h-screen bg-gray-100">
+      <div className="max-w-md bg-white p-8 shadow-md rounded-md">
+      
+        <img
+          src={image}
+          alt="Product"
+          className="w-full rounded-lg mb-4"
+        />
+
+       
+        <h2 className="text-xl font-semibold mb-2">Product Name</h2>
+
+    
+        <p className="text-gray-700 mb-4">
+          This is a detailed description of the product. Lorem ipsum dolor
+          sit amet, consectetur adipiscing elit.
+        </p>
+
+      
+        <button className="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600">
+          Add to Bag
+        </button>
+      </div>
     </div>
-  </div>
-</div>
     </>
-  )
+  );
 }
